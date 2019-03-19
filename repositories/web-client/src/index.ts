50d973913c0f88b9ca7c4a1144745c942ab052d2
András Sevcsik-Zajácz <sevcsik@sevdev.hu>
2019-03-20 00:47:54 +0100

    Edit existing repositories, disabled code formatter

--- repositories/web-client/src/index.ts
+++ repositories/web-client/src/index.ts
@@ -1,47 +1,45 @@
-import 'symbol-observable'; // Polyfill required to make Observable.from(Stream) work.
-import { mkMain, mkWebsocketClientDriver } from '@pimp/framework/client';
-import { DOMSource, makeDOMDriver } from '@cycle/dom';
-import { run } from '@cycle/rxjs-run';
+import 'symbol-observable' // Polyfill required to make Observable.from(Stream) work.
+import { mkMain
+       , mkWebsocketClientDriver
+       } from '@pimp/framework/client'
+import { DOMSource, makeDOMDriver } from '@cycle/dom'
+import { run } from '@cycle/rxjs-run'
 
-import { AnyCommand } from '../../shared/commands';
-import { AnyEvent } from '../../shared/events';
-import { AnyIntent } from '../../shared/intents';
-import { AnyReply } from '../../shared/replies';
-import { determineIntents } from './determineIntents';
-import { executeIntents } from './executeIntents';
-import { mkState, State } from './state';
-import { State as ServerState } from '../../shared/state';
-import { reducer } from './state';
-import { renderState } from './view';
-import {
-    validateCommand,
-    ValidationFailureReason
-} from '../../shared/validateCommand';
+import { AnyCommand } from '../../shared/commands'
+import { AnyEvent } from '../../shared/events'
+import { AnyIntent } from '../../shared/intents'
+import { AnyReply } from '../../shared/replies'
+import { determineIntents } from './determineIntents'
+import { executeIntents } from './executeIntents'
+import { mkState, State } from './state'
+import { State as ServerState } from '../../shared/state'
+import { reducer } from './state'
+import { renderState } from './view'
+import { validateCommand
+       , ValidationFailureReason
+       } from '../../shared/validateCommand'
 
-import { create as createSpy } from 'rxjs-spy';
+import { create as createSpy } from 'rxjs-spy'
 
-const drivers = {
-    ws: mkWebsocketClientDriver('ws://localhost:8001'),
-    dom: makeDOMDriver('#repositories-manage')
-};
+const drivers = { ws: mkWebsocketClientDriver('ws://localhost:8001')
+                , dom: makeDOMDriver('#repositories-manage')
+                }
 
-const main = mkMain<
-    AnyCommand,
-    AnyEvent,
-    AnyReply,
-    AnyIntent,
-    ValidationFailureReason,
-    ServerState,
-    State
->(
-    determineIntents,
-    executeIntents,
-    validateCommand,
-    reducer,
-    mkState,
-    renderState
-);
+const main = mkMain< AnyCommand
+                   , AnyEvent
+                   , AnyReply
+                   , AnyIntent
+                   , ValidationFailureReason
+                   , ServerState
+                   , State
+                   >( determineIntents
+                    , executeIntents
+                    , validateCommand
+                    , reducer
+                    , mkState
+                    , renderState
+                    )
 
-run(main as any, drivers);
+run(main as any, drivers)
 
-createSpy();
+createSpy()
--- repositories/web-client/src/state.ts
+++ repositories/web-client/src/state.ts
@@ -1,91 +1,71 @@
-import { AnyEvent } from '../../shared/events';
-import { AnyIntent } from '../../shared/intents';
-import { AnyReply } from '../../shared/replies';
-import { ValidationFailureReason } from '../../shared/validateCommand';
-import {
-    initialState,
-    State as ServerState,
-    reducer as eventReducer
-} from '../../shared/state';
-import {
-    Repository,
-    RepositoryFields,
-    RepositoryId
-} from '../../shared/objects';
+import { AnyEvent } from '../../shared/events'
+import { AnyIntent } from '../../shared/intents'
+import { AnyReply } from '../../shared/replies'
+import { ValidationFailureReason } from '../../shared/validateCommand'
+import { initialState
+       , State as ServerState
+       , reducer as eventReducer
+       } from '../../shared/state'
+import { Repository, RepositoryFields, RepositoryId } from '../../shared/objects'
 
-import { AnyBuiltinIntent, AnyBuiltinReply } from '@pimp/framework/client';
-import { find } from 'lodash/fp';
+import { AnyBuiltinIntent, AnyBuiltinReply } from '@pimp/framework/client'
+import { find } from 'lodash/fp'
 
-declare function assertNever(x: never): never;
+declare function assertNever(x: never): never
 
 export interface UnsavedRepository {
-    id: RepositoryId | null;
-    fields: RepositoryFields;
+    id: RepositoryId | null
+    fields: RepositoryFields
 }
 
 export interface State extends ServerState {
-    unsavedRepositories: ReadonlyArray<UnsavedRepository>;
+    unsavedRepositories: ReadonlyArray<UnsavedRepository>
 }
 
-export const mkState = (state: ServerState | null): State => ({
-    ...(state !== null ? state : initialState),
-    unsavedRepositories: []
-});
+export const mkState = (state: ServerState | null): State => ({ ...(state !== null ? state : initialState)
+                                                              , unsavedRepositories: []
+                                                              })
 
 const intentReducer = (state: State, intent: AnyIntent | AnyBuiltinIntent) => {
     switch (intent.name) {
         case 'create':
-            return {
-                ...state,
-                unsavedRepositories: [
-                    ...state.unsavedRepositories,
-                    {
-                        id: null,
-                        fields: { name: '', provider: 'bitbucket' }
-                    } as UnsavedRepository
-                ]
-            };
+            return { ...state
+                   , unsavedRepositories: [ ...state.unsavedRepositories
+                                          , { id: null
+                                            , fields: { name: '', provider: 'bitbucket' }
+                                            } as UnsavedRepository
+                                          ]
+                  }
         case 'edit':
-            const repository = find({ id: intent.id }, state.repositories);
+            const repository = find({ id: intent.id }, state.repositories)
             if (repository) {
-                return {
-                    ...state,
-                    unsavedRepositories: [
-                        ...state.unsavedRepositories,
-                        {
-                            id: intent.id,
-                            fields: {
-                                name: repository.name,
-                                provider: repository.provider
-                            }
-                        }
-                    ]
-                };
+                return { ...state
+                       , unsavedRepositories: [ ...state.unsavedRepositories
+                                              , { id: intent.id
+                                                , fields: {name: repository.name, provider: repository.provider }
+                                                }
+                                              ]
+                       }
             } else {
-                throw new Error('Cannot find repository to edit');
+                throw new Error('Cannot find repository to edit')
             }
         case 'remove':
         case 'save':
         case 'builtin view':
-            return state;
+            return state
         default:
-            return assertNever(intent);
+            return assertNever(intent)
     }
-};
+}
 
-const replyReducer = (
-    state: State,
-    message: AnyReply | AnyBuiltinReply<ServerState, ValidationFailureReason>
-) => state;
+const replyReducer = (state: State, message: AnyReply | AnyBuiltinReply<ServerState, ValidationFailureReason>) => state
 
-export const reducer = (
-    state: State,
-    message:
-        | AnyEvent
-        | AnyIntent
-        | AnyReply
-        | AnyBuiltinReply<ServerState, ValidationFailureReason>
-): State =>
+export const reducer = ( state: State
+                       , message:   AnyEvent
+                                  | AnyIntent
+                                  | AnyReply
+                                  | AnyBuiltinReply<ServerState, ValidationFailureReason>
+                      ): State =>
     // TODO: this should be in the framework
     message._type === 'event'
         ? { ...state, ...eventReducer(state, message) }
@@ -93,4 +73,4 @@ export const reducer = (
         ? intentReducer(state, message)
         : message._type === 'reply'
         ? replyReducer(state, message)
-        : assertNever(message);
+        : assertNever(message)
